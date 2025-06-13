2bd502a3745b4709b989da4d4e33ff156a9f3bce
Damian Kwolek <damiankwolek1@gmail.com>
2025-06-13 20:56:34 +0200

    add: regrowables

--- Assets/Scripts/Structure.cs
+++ Assets/Scripts/Structure.cs
@@ -1,6 +1,15 @@
 using UnityEngine;
 
-public class Structure : MonoBehaviour
+public class Structure : WorldObject
 {
+    public override bool CanInteract(GameObject interactor, Item itemUsed = null)
+    {
+        return false;
+    }
+
+    public override void Interact(GameObject interactor, Item itemUsed = null)
+    {
+        
+    }
 
 }
